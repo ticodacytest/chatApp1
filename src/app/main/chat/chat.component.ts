c3c29ce59fba90e19bfe4c0696dc6d8c18562376
Gauravbartwal <gaurav.bartwal97@gmail.com>
2021-07-20 22:09:16 +0530

    Update src/app/main/chat/chat.component.ts
    Co-authored-by: plaban1805 <87363725+plaban1805@users.noreply.github.com>

--- src/app/main/chat/chat.component.ts
+++ src/app/main/chat/chat.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
 import { AuthService } from 'src/app/services/auth.service';
 import {ApiService} from '../../services/api.service';
 
